ed5fe9be5c1dd5f61f635827ed343dd6e1bff3de
JonathanKross <kross.jonathan+github@gmail.com>
2016-04-01 10:52:50 -0400

    Adding choose_difficulty(), initial steps in main() through print(mystery word)

--- mystery_word.py
+++ mystery_word.py
@@ -3,7 +3,10 @@ import sys
 
 
 def create_list_all_words(path='/usr/share/dict/words'):
-    """Returns list of all the words in agruement path"""
+    """
+    Returns list of all the words in agruement path.
+    Path or input must contain words set to individual lines.
+    """
 
     with open(path, 'r') as file:
 
@@ -31,14 +34,14 @@ def list_easy_words(all_words):
 def list_normal_words(all_words):
     """Returns list of words only containing 6-8 characters."""
 
-    list_medium_words = []
+    list_normal_words = []
 
     for word in all_words:
 
         if len(word) >= 6 and len(word) <= 8:
-            list_medium_words.append(word)
+            list_normal_words.append(word)
 
-    return list_medium_words
+    return list_normal_words
 
 
 def list_hard_words(all_words):
@@ -59,9 +62,47 @@ def generate_random_word(all_words):
 
     return random.choice(all_words)
 
+def choose_difficulty():
+    """Prompts user to select easy, normal, or hard mode."""
+
+    return input("What mode to you want to play in? Easy[e], Normal[n], or Hard[h]: ").lower().strip()
+
+
+def guess_letter():
+    """Prompts user to for a letter to guess"""
+
+    return input("Guess a letter. ").lower().strip()
+
+
 
 def main():
-    pass
+    print("Welcome to Mystery Word Game!")
+    print("You're allowed 8 chances to guess all of the letters in the mystery word.")
+
+    difficulty = choose_difficulty()
+
+    if difficulty == 'e':
+        mystery_word = generate_random_word(list_easy_words(create_list_all_words()))
+
+    elif difficulty == 'n':
+        mystery_word = generate_random_word(list_normal_words(create_list_all_words()))
+
+    elif difficulty == 'h':
+        mystery_word = generate_random_word(list_hard_words(create_list_all_words()))
+
+    print(mystery_word)
+    # while True:
+    #
+    #     letter_ guess = guess_letter()
+    #
+    #
+    #
+    #
+    #     play_again = input("Do you want to play Mystery Word again? [y/N] \n")
+    #     if play_again.lower().strip() == "y"
+    #         main()
+    #     else:
+    #         sys.exit()
 
 if __name__ == '__main__':
     main()
