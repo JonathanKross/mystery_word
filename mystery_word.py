7baf7b75474190799900f3a6d557e0270bad30a5
JonathanKross <kross.jonathan+github@gmail.com>
2016-04-01 20:15:31 -0400

    mystery word is working. needs some debugging and clean up.

--- mystery_word.py
+++ mystery_word.py
@@ -82,55 +82,79 @@ def generate_mystery_word(difficulty):
         return generate_random_word(list_hard_words(create_list_all_words()))
 
 
-def guess_letter():
+def guess_letter(correct_guesses, incorrect_guesses):
     """Prompts user for a letter to guess"""
 
     letters = "abcdefghijklmnopqrstuvwxyz"
-    guess_attempt = input("Guess a letter: ").lower().strip()
+    while True:
 
-    if len(guess_attempt) >= 2 or guess_attempt == "":
-        print("Looks like that's not a single letter. Try again."
-        continue
+        guess_attempt = input("Guess a letter: ").lower().strip()
 
-    elif guess_attempt in correct_guesses or guess_attempt in incorrect_guesses:
-        print("Looks like you've already guessed that letter. Try again.")
-        continue
+        if len(guess_attempt) >= 2 or guess_attempt == "":
+            print("Looks like that's not a single letter. Try again.")
+            continue
 
-    elif guess_attempt not in letters:
-        print("Looks like you didn't guess a letter. Don't guess numbers or special characters.")
-        continue
+        elif guess_attempt in correct_guesses or guess_attempt in incorrect_guesses:
+            print("Looks like you've already guessed that letter. Try again.")
+            continue
 
-    else:
-        return guess_attempt
+        elif guess_attempt not in letters:
+            print("Looks like you didn't guess a letter. Don't guess numbers or special characters.")
+            continue
+
+        else:
+            return guess_attempt
+
+def display_visual_guesses(mystery_word):
+
+    for letter in mystery_word:
+        if letter in correct_guesses:
+            print(letter, end="")
+        else:
+            print("_", end="")
+
 
 
 def main():
+
+    max_number_guesses = 8
+    correct_guesses = []
+    incorrect_guesses = []
+
+    print("Welcome to Mystery Word Game!")
+    print("You're allowed 8 chances to guess all of the letters in the mystery word.")
+
+    mystery_word = generate_mystery_word(choose_difficulty())
+
+    print("Your mystery word has {} letters.\n".format(len(mystery_word)))
+
     while True:
-        print("Welcome to Mystery Word Game!")
-        print("You're allowed 8 chances to guess all of the letters in the mystery word.")
 
-        mystery_word = generate_mystery_word(choose_difficulty())
+        # display_visual_guesses(mystery_word)
 
-        print("Your mystery word has {} letters.".format(len(mystery_word)))
+        while len(incorrect_guesses) < max_number_guesses and len(correct_guesses) != len(list(mystery_word)):
 
-        guess_limit = 8
-        letters_guessed = []
-        correct_guesses = []
-        incorrect_guesses = []
-
-        while len(incorrect_guesses) < guess_limit and len(correct_guesses) != len(list(mystery_word)):
-
+            # print("_ " * len(mystery_word))
             for letter in mystery_word:
                 if letter in correct_guesses:
-                    print(letter, end="")
+                    print(letter, " ", end="")
                 else:
-                    print("_", end="")
-            print("")
-            print("Incorrect Guesses: {} out of {}".format(len(incorrect_guesses), guess_limit))
-            print("")
+                    print("_ ", end="")
 
-            letter_guess = guess_letter()
-            letters_guessed.append(letter_guess)
+            print("\n")
+
+            letter_guessed = guess_letter(correct_guesses, incorrect_guesses)
+
+            if letter_guessed in mystery_word:
+                correct_guesses.append(letter_guessed)
+                if len(correct_guesses) == len(list(mystery_word)):
+                    print("Congrats! You won! The mystery word was {}.".format(mystery_word))
+            else:
+                incorrect_guesses.append(letter_guessed)
+
+            print("\nIncorrect Guesses: {} out of {}\n".format(len(incorrect_guesses), max_number_guesses))
+            # display_visual_guesses(mystery_word, letter_guessed)
+
 
 
 
@@ -139,10 +163,14 @@ def main():
             print("You're out of guesses!. Your mystery word was {}.".format(mystery_word))
 
         play_again = input("Do you want to play Mystery Word again? [y/N] \n")
-        if play_again.lower().strip() == "y"
+
+        if play_again.lower().strip() == "y":
             main()
+
         else:
             sys.exit()
+
+
 
 
 if __name__ == '__main__':
